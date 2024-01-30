8e2c208cd9d38173e991f86a24b8bb92399b1aef
hykim6856 <hykim6856@gmail.com>
2024-01-30 13:15:21 +0900

    upload

--- 매입매출/database(2024-01-30).sql
+++ 매입매출/database(2024-01-30).sql
@@ -1,24 +1,23 @@
 -- 매입매출
 /*
-MySQL DataBase Dump
-=> Database Schema
-다른 DB에서 사용하거나
-DB를 새로 설치할 경우  사전에 기존의 사용하던 DB를
+MySQL DataBase Dump 하기
+=> Database Schema 를 통째로 파일로 백업하기
+다른 DB 에서 사용하거나
+DB를 새로 설치할 경우 사전에 기존의 사용하던 DB 를
 백업하기
 	메뉴의 Server / Data Export 를 선택하고
-    백업할 Database 와 폴더를 선택한다.
-복원히기 
-	기존의 Database Schema가 없으면
-    
-	복원할 데이터 베이스 스키마를 생성
-	메뉴의 Server / Data import 를 선택하여
+	백업할 Database 와 폴더를 선택한다
+복원하기
+	기존의 Database Schema 가 없으면
+	복원할 Database Schema 를 생성
+    메뉴의 Server / Data Import 를 선택하여
     백업파일이 저장된 폴더를 선택한다
-    백업할 때의 database Schema 와 같은 이름으로 Database를 생성해둔다.
-    
+    백업할때의 Databas Schema 와 같은 이름으로
+    Database 를 생성해 둔다
 */
-CREATE DATABASE ecountDB;
-DROP DATABASE ecountdb;
-DROP DATABASE newschema;
+DROP DATABASE ecountDB;
+CREATE DATABASE ecountdb;
+
 
 USE ecountDB;
 SHOW TABLES;
@@ -35,7 +34,7 @@ tbl_iolist table 의 데이터는 정규화(3정규화)가 완료된 상태이�
  
  -- 무조건적으로 두 테이블을 Join 하여 보기
  select *
- from tbl_iolist, tbl_product
+ from tbl_iolist, tbl_products
  where io_pcode = p_code;
  
  -- 원하는 모양대로 조회결과를 확인하기 위해서는 
